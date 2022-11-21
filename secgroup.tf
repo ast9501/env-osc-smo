ed2f1518017ac2510f6ba96550d1e8fb885ab852
ast9501 <ast9501.cs10g@nctu.edu.tw>
2022-11-21 21:39:26 +0800

    chore: update security group for k8s and smo ports

--- secgroup.tf
+++ secgroup.tf
@@ -20,3 +20,43 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_osc_icmp" {
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = "${openstack_networking_secgroup_v2.secgroup_osc.id}"
 }
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_osc_odlweb" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 30205
+  port_range_max    = 30205
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_osc.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_osc_jenkins" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 32080
+  port_range_max    = 32080
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_osc.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_osc_kubernetes_api" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 6443
+  port_range_max    = 6443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_osc.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_osc_kubernetes_kubelet" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 10250
+  port_range_max    = 10250
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_osc.id}"
+}
